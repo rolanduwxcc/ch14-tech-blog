e9a74c67f274a7abb3036dbf2f98f2a3e78f9b0f
rolanduwxcc <rolanduwxcc@gmail.com>
2021-03-21 00:47:39 -0500

    couple of templates added

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -38,6 +38,46 @@ router.get('/', (req, res) => {
     });
 });
 
+// router.get('/:id', (req, res) => {
+//     console.log(req.session);
+
+//     Post.findAll({
+//         where: {
+//             // id: req.params.id
+//             user_id: 7
+//           },
+//         attributes: [
+//             'id',
+//             'title',
+//             'content',
+//             'created_at'
+//         ],
+//         include: [
+//             {
+//                 model: Comment,
+//                 attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+//                 include: {
+//                     model: User,
+//                     attributes: ['username']
+//                 }
+//             },
+//             {
+//                 model: User,
+//                 attributes: ['username']
+//             }
+//         ]
+//     }).then(dbPostData => {
+//         //pass a single post object into the homepage template
+//         // console.log(dbPostData[0]);
+//         const posts = dbPostData.map(post => post.get({ plain: true }));
+//         res.render('homepage', { posts, loggedIn: req.session.loggedIn });
+//     })
+//     .catch(err => {
+//         console.log(err);
+//         res.status(500).json(err);
+//     });
+// });
+
 router.get('/login', (req,res) => {
     if (req.session.loggedIn) {
         res.redirect('/');
